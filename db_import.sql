6217c8fe152b141b9e4d99b7dce55d3bad9a0eb3
mongooselabs@gmail.com <mongooselabs@gmail.com>
2016-02-05 14:13:43 +0700

    correct scrapy.cfg file

--- db_import.sql
+++ db_import.sql
@@ -1,2 +1,2 @@
 
-mysql -u root -p88footbDb#836 productinfo < ~/git-projects/productz/productinfo/db/companyinfo.sql
+mysql -u root -p88footbDb#836 productinfo < ~/git-projects/productz/productinfo/db/productinfo.sql
