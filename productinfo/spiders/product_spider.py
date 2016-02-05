713b33bb895af65e15f94950bf43c1f9e86c6832
mongooselabs@gmail.com <mongooselabs@gmail.com>
2016-02-05 16:27:10 +0700

    sitemap crawler

--- productinfo/spiders/product_spider.py
+++ productinfo/spiders/product_spider.py
@@ -4,36 +4,37 @@ import re
 from scrapy.spiders import SitemapSpider
 
 #from bs4 import BeautifulSoup
-from productinfo.items import ProductItem
-from productinfo.items import CategoryItem
-from productinfo.items import ProductCategoryItem
-from productinfo.items import BranchItem
-from productinfo.items import ProductBranchItem
-from productinfo.items import SupplierItem
-from productinfo.items import ProductSupplierItem
-from productinfo.items import UrlFailureItem
+from productinfo.items import *
 
 
 class ProductSpider(SitemapSpider):
     
-    name = "ProductSpider"
-    allowed_domains = ["http://www.lazada.vn",
+    name = "product"
+    allowed_domains = ["http://www.lazada.vn/",
                        "http://www.adayroi.vn",
                        "http://www.tiki.vn"]
-    sitemap_urls = ['http://www.lazada.vn/sitemap.xml'
+    start_urls = ['http://www.lazada.vn/sitemap.xml'
                     ]
     
-    sitemap_rules = [('/product/', 'parse_product')]
+    #sitemap_rules = [('/product/', 'parse_product')]
+    #sitemap_follow = ['/sitemap-products']
     
     root_url = 'http://lazada.vn/'
     root_page = root_url + ''
-    
-    def __init__(self):
-    
-        pass
 
     def parse(self, response):
-        print response
+        nodename = 'loc'
+        text = body_or_str(response)
+        r = re.compile(r"(<%s[\s>])(.*?)(</%s>)" % (nodename, nodename), re.DOTALL)
+        print response.body
+        for match in r.finditer(text):
+            url = match.group(2)
+            print url
+            yield Request(url, callback=self.parse_page)
+    
+    def parse_page(self, response):
+        print response.url
+        print response.body
         pass
     
     def parse_product(self, response):
