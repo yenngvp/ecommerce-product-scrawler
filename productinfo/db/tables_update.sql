9292b6fef8ebf280bdcd2190c92f571d1eaa49fe
YenNguyenBB <mongooselabs@gmail.com>
2016-02-16 00:42:31 +0700

    [Lazada] db update

--- productinfo/db/tables_update.sql
+++ productinfo/db/tables_update.sql
@@ -1,36 +1,16 @@
--- MySQL dump 10.13  Distrib 5.7.9, for osx10.9 (x86_64)
---
--- Host: localhost    Database: productinfo
--- ------------------------------------------------------
--- Server version	5.7.10
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+-- Add url column to tmp_product table
+ALTER TABLE `productinfo`.`tmp_product`
+ADD COLUMN `url` VARCHAR(256) NULL AFTER `timestamp`,
+ADD UNIQUE INDEX `url_UNIQUE` (`url` ASC);
 
---
--- Dumping data for table `tmp_product_link`
---
-
-LOCK TABLES `tmp_product_link` WRITE;
-/*!40000 ALTER TABLE `tmp_product_link` DISABLE KEYS */;
-/*!40000 ALTER TABLE `tmp_product_link` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2016-02-15 23:00:22
+-- Create table tmp_product_link
+CREATE TABLE `tmp_product_link` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `source` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `url` varchar(256) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `update_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `create_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `changefreq` varchar(10) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
